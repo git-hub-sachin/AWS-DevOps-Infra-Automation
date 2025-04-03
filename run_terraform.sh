39e82c8e60939ca6fcca2ab216d345b926a2b1ae
Sachin <sc626361@gmail.com>
2025-04-03 11:10:56 +0530

    update

--- run_terraform.sh
+++ run_terraform.sh
@@ -1,12 +1,44 @@
-#!/bin/bash
+# #!/bin/bash
+# set -e
+
+# ACTION=$1
+# ENVIRONMENT=$2
+
+# case $ACTION in
+#   "init")
+#     terraform init
+#     ;;
+#   "plan")
+#     terraform plan -var-file="${ENVIRONMENT}.tfvars" -out=tfplan
+#     ;;
+#   "apply")
+#     terraform apply "tfplan"
+#     ;;
+#   "destroy")
+#     terraform destroy -var-file="${ENVIRONMENT}.tfvars"
+#     ;;
+#   *)
+#     echo "Invalid action. Use 'init', 'plan', 'apply', or 'destroy'."
+#     exit 1
+#     ;;
+# esac
+
+
 set -e
 
 ACTION=$1
 ENVIRONMENT=$2
 
+# S3 backend configuration
+TF_BUCKET="my-terraform-state-docker-image"
+TF_STATE_KEY="terraform.tfstate"
+TF_REGION="us-west-1"
+
 case $ACTION in
   "init")
-    terraform init
+    terraform init -backend-config="bucket=$TF_BUCKET" \
+                   -backend-config="key=$TF_STATE_KEY" \
+                   -backend-config="region=$TF_REGION"
     ;;
   "plan")
     terraform plan -var-file="${ENVIRONMENT}.tfvars" -out=tfplan
